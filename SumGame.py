bd8a97c1701efbfb16e4bd0f33e3f828577884bc
JaskaranSingh2 <jasonkaura@gmail.com>
2022-02-16 14:53:55 -0700

    com

--- SumGame.py
+++ SumGame.py
@@ -11,12 +11,13 @@ x = 0
 while True:
     check = runs1[x * i] + runs1[i]
     check2 = runs2[x * i] + runs2[i]
-    if i > len(runs1) | i > len(runs2) | (x * i) > len(runs1) | (x * i) > len(runs2):
-        print(0)
-    elif check == check2:
+    if check == check2:
         equalDay = len(range(0, i + 1))
         break
+    elif bool(runs1[i] | runs2[i] | runs1[x * i] | runs2[x * i]) == False:
+        print(0)
+        break
     else:
-        i += 1
         x += 1
+        i += 1
 print(equalDay)
