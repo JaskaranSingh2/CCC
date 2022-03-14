7cac40ce90b204bf069d0c45f8fe3d48839ed94b
JaskaranSingh2 <jasonkaura@gmail.com>
2022-03-14 14:50:34 -0600

    com

--- UpAndDown.py
+++ UpAndDown.py
@@ -6,3 +6,28 @@ sTotal = int(input())
 
 countAB = 0
 countCD = 0
+stepsAB = 0
+stepsCD = 0
+while countAB != sTotal:
+    if sTotal - countAB < countAB:
+        countAB += sTotal - countAB
+    else:
+        countAB += aSteps + bSteps
+        stepsAB += aSteps - bSteps
+    
+while countCD != sTotal:
+    if sTotal - countCD >= countCD:
+        countCD += sTotal - countCD
+    else:
+        countCD += cSteps + dSteps
+        stepsCD += cSteps - dSteps
+
+print(stepsAB)
+print(stepsCD)
+
+if stepsAB > stepsCD:
+    print("Nikky")
+elif stepsAB == stepsCD:
+    print("Tied")
+else:
+    print("Byron")
