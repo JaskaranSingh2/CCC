14c49517221b831285a017bd2f5d0367ec4fbf32
Jaskaran Singh <jasonkaura@gmail.com>
2022-03-15 19:25:01 +0000

    com

--- UpAndDown.py
+++ UpAndDown.py
@@ -1,33 +1,47 @@
-aSteps = int(input())
-bSteps = int(input())
-cSteps = int(input())
-dSteps = int(input())
-sTotal = int(input())
+# Person 1
+# a steps forward, b steps backward
 
-countAB = 0
-countCD = 0
-stepsAB = 0
-stepsCD = 0
-while countAB != sTotal:
-    if sTotal - countAB < countAB:
-        countAB += sTotal - countAB
+# Person 2
+# c steps forward, d steps backward
+
+# a >= b
+# c >= d
+# Same total number of steps, not same distance
+# Nicky and Byron both step forward on their first steps at the same time
+
+a = int(input())
+b = int(input()) 
+c = int(input())
+d = int(input())
+S = int(input()) # Total length of step
+
+
+positionFromStart1 = a - b
+currentTotal1 = a + b
+positionFromStart2 = c - d
+currentTotal2 = c + d
+
+while currentTotal1 != S:
+    currentTotal1 += (a + b) 
+    positionFromStart1 += (a - b)
+
+print(currentTotal1)
+print(positionFromStart1)
+
+while currentTotal2 != S:
+    if currentTotal2 + (c+d) > S:
+        positionFromStart2 += S - currentTotal2
+        break
     else:
-        countAB += aSteps + bSteps
-        stepsAB += aSteps - bSteps
-    
-while countCD != sTotal:
-    if sTotal - countCD >= countCD:
-        countCD += sTotal - countCD
-    else:
-        countCD += cSteps + dSteps
-        stepsCD += cSteps - dSteps
+        currentTotal2 += (c+d)
+        positionFromStart2 += (c - d)     
 
-print(stepsAB)
-print(stepsCD)
-
-if stepsAB > stepsCD:
-    print("Nikky")
-elif stepsAB == stepsCD:
-    print("Tied")
-else:
-    print("Byron")
+print(currentTotal2)
+print(positionFromStart2)
+"""
+what I need to do:
+The position from start for one sequence is a-b/c-d
+The total number of steps is a+b/c+d while it does not equal S
+The total number of steps must not exceed S
+If a+b/c+d adds to make the total number of steps greater than S, then we will add S - total number of steps
+"""
