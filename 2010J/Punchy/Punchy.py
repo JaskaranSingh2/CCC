61ad573ad4ce1fb390d88d35afaf0ddf6acfc543
JaskaranSingh2 <jasonkaura@gmail.com>
2022-03-17 14:14:01 -0600

    com

--- 2010J/Punchy/Punchy.py
+++ 2010J/Punchy/Punchy.py
@@ -12,6 +12,35 @@ while i < 6:
 print(store)
 
 for i in range(len(store)): # for i in the range of the length of the list
-        if store[i][4] == "a":
-                print("yes")
-                break
+        if store[i][0] == "1":
+                if store[i][2] == "A":
+                        A = int(store[i][4])
+                else:
+                        B = int(store[i][4])
+        elif store[i][0] == "2":
+                if store[i][2] == "A":
+                        print(A)
+                else:
+                        print(B)
+        elif store[i][0] == "3":
+                if store[i][2] == "A":
+                        A = A + B
+                else:
+                        B = B + A
+        elif store[i][0] == "4":
+                if store[i][2] == "A":
+                        A = A * B
+                else:
+                        B = B * A
+        elif store[i][0] == "5":
+                if store[i][2] == "A":
+                        A = A - B
+                else:
+                        B = B - A
+        elif store[i][0] == "6":
+                if store[i][2] == "A":
+                        A = math.floor(A / B)
+                else:
+                        B = math.floor(B / A)
+        elif store[i][0] == "7":
+                exit()
