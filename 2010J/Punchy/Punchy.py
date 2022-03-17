cae75374295e092d6d52a5a60242abfffa1e6ca3
JaskaranSingh2 <jasonkaura@gmail.com>
2022-03-17 14:16:20 -0600

    com

--- 2010J/Punchy/Punchy.py
+++ 2010J/Punchy/Punchy.py
@@ -11,36 +11,36 @@ while i < 6:
         i+=1
 print(store)
 
-for i in range(len(store)): # for i in the range of the length of the list
-        if store[i][0] == "1":
-                if store[i][2] == "A":
-                        A = int(store[i][4])
+for x in range(len(store)): # for x in the range of the length of the list
+        if store[x][0] == "1":
+                if store[x][2] == "A":
+                        A = int(store[x][4])
                 else:
-                        B = int(store[i][4])
-        elif store[i][0] == "2":
-                if store[i][2] == "A":
+                        B = int(store[x][4])
+        elif store[x][0] == "2":
+                if store[x][2] == "A":
                         print(A)
                 else:
                         print(B)
-        elif store[i][0] == "3":
-                if store[i][2] == "A":
+        elif store[x][0] == "3":
+                if store[x][2] == "A":
                         A = A + B
                 else:
                         B = B + A
-        elif store[i][0] == "4":
-                if store[i][2] == "A":
+        elif store[x][0] == "4":
+                if store[x][2] == "A":
                         A = A * B
                 else:
                         B = B * A
-        elif store[i][0] == "5":
-                if store[i][2] == "A":
+        elif store[x][0] == "5":
+                if store[x][2] == "A":
                         A = A - B
                 else:
                         B = B - A
-        elif store[i][0] == "6":
-                if store[i][2] == "A":
+        elif store[x][0] == "6":
+                if store[x][2] == "A":
                         A = math.floor(A / B)
                 else:
                         B = math.floor(B / A)
-        elif store[i][0] == "7":
+        elif store[x][0] == "7":
                 exit()
