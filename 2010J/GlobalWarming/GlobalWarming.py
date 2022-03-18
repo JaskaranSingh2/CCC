01fbe5eb298f479b9006861aa2cbec92383bccf2
JforJason101 <jasonkaura@gmail.com>
2022-03-17 23:44:14 -0600

    com

--- 2010J/GlobalWarming/GlobalWarming.py
+++ 2010J/GlobalWarming/GlobalWarming.py
@@ -3,3 +3,10 @@ for x, y, z in d:
     print(x, y, z)
 
 # unpacking tuples in a list
+
+numbers = [12, 23, 3, 4, 52, 6]
+
+copy = [ALL for ALL in numbers]
+print(copy)
+greater = [gThanTen for gThanTen in numbers if gThanTen > 10]
+print(greater)
