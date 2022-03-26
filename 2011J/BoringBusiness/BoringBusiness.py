fa58f92cdc8dfb812ac5c8a0ac1c866194a4783a
Jaskaran Singh <jasonkaura@gmail.com>
2022-03-26 00:58:51 +0000

    com

--- 2011J/BoringBusiness/BoringBusiness.py
+++ 2011J/BoringBusiness/BoringBusiness.py
@@ -3,10 +3,7 @@ I had a version that worked for all cases except the last one, because the time 
 so I had to implement the error handling @sjay05 on GitHub used. Again, the early CCC solutions are practice for me.
 """
 
-from this import d
-
-
-visited = [ 
+grid = [ 
 [0, -1], [0, -2], [0, -3], [1, -3], [2, -3], 
 [3, -3], [3, -4], [3, -5], [4, -5], [5, -5], 
 [5, -4], [5, -3], [6, -3], [7, -3], [7, -4], 
@@ -14,6 +11,39 @@ visited = [
 [4, -7], [3, -7], [2, -7], [1, -7], [0, -7], 
 [-1, -7], [-1, -6], [-1, -5]
 ]
+
+def calcNext(inputs):
+    x = -1
+    y = -5
+    error = False # Initial assumption
+    for i in range(len(inputs)):
+        way = inputs[i][0]
+        distance = int(inputs[i][1])
+        if way == "l":
+            for i in range(1, distance+1):
+                if [x-i, y] in grid:
+                    error = True
+            coordinate = [x-distance, y]
+        elif way == "r":
+            for i in range(1, distance+1):
+                if [x+i, y] in grid:
+                    error = True
+            coordinate = [x+distance, y]
+        elif way == "u":
+            for i in range(1, distance+1):
+                if [x, y+i] in grid:
+                    error = True
+            coordinate = [x, y+distance]
+        elif way == "d":
+            for i in range(1, distance+1):
+                if [x, y-i] in grid:
+                    error = True
+            coordinate = [x, y-distance]   
+    if error:
+        print(coordinate[0], coordinate[1], "DANGER")
+        exit()
+    else:
+        print(coordinate[0], coordinate[1], "safe")
 
 # append directions to a list
 inputs = []
@@ -24,15 +54,10 @@ while True:
         break
     else:
         inputs.append(direction)
+        calcNext(inputs)
+
 
 # error-proof it
+# notes: check if each increment in the direction is valid
+# if it is invalid, the error = True
 
-def calcNext(direction):
-    x = -1
-    y = -5
-    error = False
-    for i in range(len(direction)):
-        way = direction[i][0]
-    # notes: check if each increment in the direction is valid
-    # if it is invalid, the error = True
-    
