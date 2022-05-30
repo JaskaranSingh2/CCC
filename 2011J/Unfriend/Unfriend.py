c67cd4d4fdbd80082cc939e90c9edbefec4708db
Jaskaran <32575402+JaskaranSingh2@users.noreply.github.com>
2022-05-30 19:32:45 +0000

    com

--- 2011J/Unfriend/Unfriend.py
+++ 2011J/Unfriend/Unfriend.py
@@ -1,10 +1,15 @@
+from pyparsing import traceParseAction
+
+
 N = int(input())
 N = N if N <= 6 else exit()
 
-# when something is removed, all of the things they invited are removed
-# we need to track the combinations
-# 2 combos are certain {none of them} or {all of them}
-# the other combos is {some of them}, this one can be restrictive and non-restrictive
-# let's tackle the restrictive one first
-
-count = 0 # count all combos
+tracker = {
+    
+}
+lineN = 0
+for i in (range(1, N)):
+    lineN += 1
+    tracker[lineN] = []
+    tracker[lineN].append(int(input()))
+    print(tracker[lineN])
