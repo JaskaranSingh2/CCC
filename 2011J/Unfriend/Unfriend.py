d3ffff2d87b8be8e5bdcaa2c24d7ce3abb65f42f
Jaskaran Singh <jasonkaura@gmail.com>
2022-06-13 19:06:02 +0000

    com

--- 2011J/Unfriend/Unfriend.py
+++ 2011J/Unfriend/Unfriend.py
@@ -1,15 +1,23 @@
-from pyparsing import traceParseAction
-
 
 N = int(input())
 N = N if N <= 6 else exit()
 
-tracker = {
-    
-}
+tracker = {}
+
 lineN = 0
 for i in (range(1, N)):
     lineN += 1
     tracker[lineN] = []
     tracker[lineN].append(int(input()))
     print(tracker[lineN])
+
+visited = set()
+
+def dfs(visited, tracker, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in tracker[node]:
+            dfs(visited, tracker, neighbour)
+
+dfs(visited, tracker, 1)
